32cb48c43de7cd0895b24454ea7e5840395ad4e1
Tim Brackfield <Cloud_193@hotmail.com>
2018-05-18 12:35:14 +0100

    fixed typing

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -1,12 +1,12 @@
-// app/javascript/components/banner.js
-import Typed from 'typed.js';
+// // app/javascript/components/banner.js
+// import Typed from 'typed.js';
 
-function loadDynamicBannerText() {
-  new Typed('#banner-typed-text', {
-    strings: ["Drink some weird stuff", "Get shitfaced"],
-    typeSpeed: 50,
-    loop: true
-  });
-}
+// function loadDynamicBannerText() {
+//   new Typed('#banner-typed-text', {
+//     strings: ["Drink some weird stuff", "Get shitfaced"],
+//     typeSpeed: 50,
+//     loop: true
+//   });
+// }
 
-export { loadDynamicBannerText };
+// export { loadDynamicBannerText };
