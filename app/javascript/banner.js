9b93ebb1ab8c7d8880e69eb6b07743f1045d3bb3
Tim Brackfield <Cloud_193@hotmail.com>
2018-05-18 17:36:44 +0100

    minor styling

--- app/javascript/banner.js
+++ app/javascript/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Drink some weird stuff", "Get shitfaced", "Create some mayhem"],
+    strings: ["Drink some weird stuff", "Get shitfaced", "Create some mayhem", "Please drink responsibly...", "Only joking"],
     typeSpeed: 50,
     loop: true
   });
